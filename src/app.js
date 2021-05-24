f794b70f11ba91bc744e3168ead2e6faa2fa081b
sandro scheffmann <sandro.scheffmann@personize.co.jp>
2021-05-24 22:28:22 +0900

    http.createServer

--- src/app.js
+++ src/app.js
@@ -3,7 +3,13 @@ import cron from 'node-cron';
 import { sendMail } from './mailer/index.js';
 import { checkWebsites } from './scraping/index.js';
 
-http.createServer(onRequest).listen(process.env.PORT || 6000);
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Hello World!');
+    res.end();
+  })
+  .listen(process.env.PORT || 6000);
 
 cron.schedule('0 0 */1 * * *', () => {
   checkWebsites();
